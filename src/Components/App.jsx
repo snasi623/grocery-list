2779da936640f93ef9d5afc299e7f040b76ff2b6
snasi623 <snasi1994@gmail.com>
2021-11-11 18:20:50 -0600

    deleted images

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,9 +8,7 @@ function App() {
     return (
         <div>
             <div className="header">
-                <img src="/images/icon.png" alt="Fridge Logo" className="icon"/>
                 <h1>Fridge Friend</h1>
-                <img src="/images/icon.png" alt="Fridge Logo" className="icon"/>
             </div>        
             <div className="instructions">Search for a meal you want to cook this week to get a list of groceries you will need to buy. Right click on the meal to open a copy of the recipe for reference.</div>    
             <TabList>
